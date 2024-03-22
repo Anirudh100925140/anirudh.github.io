a2cba31069e98065bce8a32e0bd664e1ffa51965
Anirudh100925140 <156684680+Anirudh100925140@users.noreply.github.com>
2024-03-21 23:31:56 -0400

    CREATED AN UPDATE FUNCTION
    if the x and y position is greater than or less than the browser viewport then the balls turn in another direction

--- Assignments/Assignment4/Part-3/main.js
+++ Assignments/Assignment4/Part-3/main.js
@@ -23,8 +23,8 @@ class Ball {
   constructor (x, y, velX, velY, color, size){
     this.x = x //horizontal position
     this.y = y; //vertical osition
-    this.velX = velX; //velocity x added to codinate x  when we animate our ball
-    this.velY = velY;//velocity y added to codinate y
+    this.velx = velx; //velocity x added to codinate x  when we animate our ball
+    this.vely = vely;//velocity y added to codinate y
     this.color = color; // fill ball shape with given colour.
     this.size = size; //radious of ball 
   }
@@ -32,8 +32,18 @@ class Ball {
   drawBall(){
     ctx.beginPath(); //starts to draw
     ctx.fillStyle = this.color; //fills color
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);//a simple math fundyion.
+    ctx.fill();//finish drawing
+
+  }
+  //create update function
+  updateBall(){
+    if(this.x + this,size >= width || this.x - this.size <=0){
+      this.velx = this.velx;
+    }
+    if(this.y + this,size >= height || this.y - this.size <=0){
+      this.vely = this.vely;
+    }
 
   }
 }
