b17d778a5152bd37973a08cce77a634fe44c6247
Anirudh100925140 <156684680+Anirudh100925140@users.noreply.github.com>
2024-03-22 11:43:41 -0400

    Wiring up the Darken/Lighten button

--- Assignments/Assignment4/Part-2/main.js
+++ Assignments/Assignment4/Part-2/main.js
@@ -30,15 +30,15 @@ newImage.addEventListener('click', e => {
 }
 
 /* Wiring up the Darken/Lighten button */
-btn.onclick = function(){
-    if (btn.getAttribute('class') === 'dark'){
-        btn.setAttribute('class', 'light');
-        btn.textContent = 'Lighten'; 
-        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    } else {   
-        btn.setAttribute('class', 'dark');
-        btn.textContent = 'Darken';
-        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
     }
-
-}
+  });
