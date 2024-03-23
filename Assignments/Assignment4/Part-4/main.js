b4a9f3d14821fcdd27d026b6b6d4cb7db0f021f3
Anirudh100925140 <156684680+Anirudh100925140@users.noreply.github.com>
2024-03-22 22:41:30 -0400

    Update main.js
    collisionDetect added

--- Assignments/Assignment4/Part-4/main.js
+++ Assignments/Assignment4/Part-4/main.js
@@ -130,6 +130,22 @@ checkBounds() {
     this.y += this.size;
   }
 }
+collisionDetect() {
+  for (const ball of balls) {
+    if (ball.exists) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + ball.size) {
+        ball.exists = false;
+        count--;
+        countDisplay.textContent = 'Ball count: ' + count;
+      }
+    }
+  }
+}
+
 
  
 
